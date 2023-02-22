19a6ef843cc16edf7ef86b08ad9d0bdcaeffc74b
joseLeiva <106642804+JLeiva44@users.noreply.github.com>
2023-02-22 03:52:21 +0100

    crud done

--- SGI/Apps/Indicadores/urls.py
+++ SGI/Apps/Indicadores/urls.py
@@ -12,10 +12,21 @@ urlpatterns = [
     path('actualizarindicador/<pk>/',views.Indicador_Update.as_view(), name = 'actualizar_indicador'),
     path('eliminarindicador/<pk>/',views.Indicador_Delete.as_view(), name = 'eliminar_indicador'),
 
+    path('listarvalor/',views.Valor_Indicador_List.as_view(), name = 'listar_valor_indicador'),
+    path('crearvalor/',views.Valor_Indicador_Create.as_view(), name = 'crear_valor_indicador'),
+    path('actualizarvalor/<pk>',views.Valor_Indicador_Update.as_view(), name = 'actualizar_valor_indicador'),
+    path('eliminarvalor/<pk>',views.Valor_Indicador_Delete.as_view(), name = 'eliminar_valor_indicador'),
+    
+
+    path('listaralarma/',views.Alarma_List.as_view(), name = 'listar_alarma'),
+    path('crearalarma/',views.Alarma_Create.as_view(), name = 'crear_alarma'),
+    path('eliminaralarma/<pk>',views.Alarma_Delete.as_view(), name = 'eliminar_alarma'),
+    path('actualizaralarma/<pk>',views.Alarma_Update.as_view(), name = 'actualizar_alarma'),
+
  
 
-    path('alarmaform/', views.alarma_form, name = 'crear_alarma'),
-    path('medidaform/', views.medida_form, name = 'crear_medida'),
-    path('valorindicadorform/', views.valor_indicador_form, name = 'crear_valor_de_indicador'),
+    # path('alarmaform/', views.alarma_form, name = 'crear_alarma'),
+    # path('medidaform/', views.medida_form, name = 'crear_medida'),
+    # path('valorindicadorform/', views.valor_indicador_form, name = 'crear_valor_de_indicador'),
     path('base/',views.base, name= 'base'),
 ]
--- SGI/Apps/Indicadores/views.py
+++ SGI/Apps/Indicadores/views.py
@@ -1,14 +1,15 @@
 from re import L
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from Apps.Indicadores.form import Alarma_Form, Valor_Indicador_Form, Medida_Form,Indicador_Form
-from Apps.Indicadores.models import Indicador
+from Apps.Indicadores.models import Indicador,Valor_Indicador, Alarma, Medida
 # Create your views here.
 
 # vistas basadas en clases
 
+# Indicador..........
 class Indicador_List(ListView):
     model = Indicador
     template_name: str = 'indicadores/indicador_list.html'
@@ -22,10 +23,9 @@ class Indicador_Create(CreateView):
 class Indicador_Update(UpdateView):
     model = Indicador
     fields = '__all__'
-    form_class: Indicador_Form
+    form_class = Indicador_Form
     template_name: str = 'indicadores/indicador_form.html'
     success_url = reverse_lazy('indicadores:listar_indicador')
-
 
 
 class Indicador_Delete(DeleteView):
@@ -33,6 +33,84 @@ class Indicador_Delete(DeleteView):
     fields = '__all__'   
     template_name: str = 'indicadores/indicador_delete.html'
     success_url = reverse_lazy('indicadores:listar_indicador')
+
+
+
+# Valor Indicador
+class Valor_Indicador_List(ListView):
+    model = Valor_Indicador
+    template_name: str = 'indicadores/valor_indicador/valor_indicador_list.html'
+
+class Valor_Indicador_Create(CreateView):
+    model = Valor_Indicador
+    template_name: str = 'indicadores/valor_indicador/valor_indicador_form.html'
+    form_class = Valor_Indicador_Form
+    #second_form_class = Indicador_Form
+    success_url= reverse_lazy('indicadores:listar_valor_indicador')
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(Valor_Indicador_Create, self).get_context_data(**kwargs)
+    #     if 'form' not in context:
+    #         context['form'] = self.form_class(self.request.GET)
+    #     if 'form2' not in context:
+    #         context['form2'] = self.second_form_class(self.request.GET)
+    #     return context  
+
+    # def post(self, request, *args, **kwargs):
+    #     self.object = self.get_object
+    #     form = self.form_class(request.POST)
+    #     form2 = self.second_form_class(request.POST)
+
+    #     if form.is_valid() and form2.is_valid():
+    #         solicitud = form.save(commit = False)
+    #         solicitud.persona = form2.save() 
+    #         solicitud.save()
+    #         return HttpResponseRedirect(self.get_success_url())
+    #     else :
+    #         return self.render_to_response(self.get_context_data(form = form, form2 = form))         
+
+
+class Valor_Indicador_Update(UpdateView):
+    model = Valor_Indicador
+    # fields = '__all__'
+    form_class = Valor_Indicador_Form
+    template_name: str = 'indicadores/valor_indicador/valor_indicador_form.html'
+    success_url = reverse_lazy('indicadores:listar_valor_indicador')    
+
+
+class Valor_Indicador_Delete(DeleteView):
+    model = Valor_Indicador
+    fields = '__all__'
+    template_name: str = 'indicadores/valor_indicador/valor_indicador_delete.html'
+    success_url = reverse_lazy('indicadores:listar_valor_indicador')    
+
+
+
+class Alarma_Create(CreateView):
+    model = Alarma
+    form_class = Alarma_Form
+    template_name: str = 'indicadores/alarma/alarma_form.html'
+    success_url = reverse_lazy('indicadores:listar_alarma')
+
+class Alarma_Delete(DeleteView):
+    model = Alarma
+    fields = '__all__'
+    template_name: str = 'indicadores/alarma/alarma_delete.html'
+    success_url = reverse_lazy('indicadores:listar_alarma')    
+
+
+class Alarma_List(ListView):
+    model = Alarma
+    template_name: str = 'indicadores/alarma/alarma_list.html'
+
+class Alarma_Update(UpdateView):
+    model = Alarma
+    # fields = '__all__'
+    form_class = Alarma_Form
+    template_name: str = 'indicadores/alarma/alarma_form.html'
+    success_url = reverse_lazy('indicadores:listar_alarma')    
+
+
 
 # vistas basadas en funciones
 
